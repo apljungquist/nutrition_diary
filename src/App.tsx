68694f36be6326a1b0239a4b4a4e68b3fb7698c2
AP Ljungquist <ap@ljungquist.eu>
2021-01-04 14:15:41 +0100

    TMP: Replace react placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Navbar expand="lg" variant="light" bg="light">
+                    <Navbar.Brand href="#">NutritionDiary</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#fast">Fast</Nav.Link>
+                        <Nav.Link href="#eat">Eat</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
